be4c7fe952e15c2571b822007c651f71ff210adb
hessphil <p.hess1@gmx.de>
2017-05-16 12:17:14 +0200

    add chat example

--- newsAPI/getNews.js
+++ newsAPI/getNews.js
@@ -1,3 +1,11 @@
+var newsMap = new Map([["spiegel","https://newsapi.org/v1/articles?source=spiegel-online&sortBy=top&apiKey=298ff739e4364254a522a26fc88466da"],
+["bild","https://newsapi.org/v1/articles?source=bild&sortBy=latest&apiKey=298ff739e4364254a522a26fc88466da"],
+["focus","https://newsapi.org/v1/articles?source=focus&sortBy=top&apiKey=298ff739e4364254a522a26fc88466da"],
+["tagesspiegel","https://newsapi.org/v1/articles?source=der-tagesspiegel&sortBy=latest&apiKey=298ff739e4364254a522a26fc88466da"],
+["zeit","https://newsapi.org/v1/articles?source=die-zeit&sortBy=latest&apiKey=298ff739e4364254a522a26fc88466da"]]);
+
+var  response = [];
+
 // Create the XHR object.
 function createCORSRequest(method, url) {
   var xhr = new XMLHttpRequest();
@@ -16,36 +24,42 @@ function createCORSRequest(method, url) {
 }
 
 var validNews;
-var filter="FC Bayern";
+var filter="Lindner";
 
 function getNews(){
-	var url1='https://newsapi.org/v1/articles?source=bild&sortBy=latest&apiKey=298ff739e4364254a522a26fc88466da';
-	var url2='https://newsapi.org/v1/articles?source=spiegel-online&sortBy=top&apiKey=298ff739e4364254a522a26fc88466da';
-	var url3='https://newsapi.org/v1/articles?source=focus&sortBy=top&apiKey=298ff739e4364254a522a26fc88466da';
-	
-	getTheLatest(url1);
-	getTheLatest(url2);
-	getTheLatest(url3);
-
+	response = [];
+	for (let url of newsMap){
+		getTheLatest(url[1]);
+	}
 }
 
 function getTheLatest(url){
 	var xhr = createCORSRequest('GET', url);
 	xhr.send();
-	var response
 	// Response handlers.
 	xhr.onload = function() {
-    response = xhr.responseText;
-	response = JSON.parse(response);
-	var arrayLength = response.articles.length;
-	for (var i = 0; i < arrayLength; i++)
-	{
-		if(response.articles[i].description.includes(filter)){
-			responsiveVoice.speak(response.articles[i].title,"Deutsch Female");
-			responsiveVoice.speak(response.articles[i].description,"Deutsch Female");
-			responsiveVoice.speak("Du möchtest mehr erfahren? Dann klick weiter","Deutsch Female");
-		}
-		
-	}
+    var string = xhr.responseText;
+	var temp = JSON.parse(string);
+	response.push(temp);
+	
+	
 	};
+}
+
+function playTheNews(){
+	var readNews = response;
+	var responseLength = readNews.length;
+	for (var a=0; a < responseLength; a++)
+	{
+		var arrayLength = readNews[a].articles.length;
+		for (var i = 0; i < arrayLength; i++)
+		{
+			if(readNews[a].articles[i].description.includes(filter)){
+				responsiveVoice.speak(readNews[a].articles[i].title,"Deutsch Female");
+				responsiveVoice.speak(readNews[a].articles[i].description,"Deutsch Female");
+				responsiveVoice.speak("Du möchtest mehr erfahren? Dann klick weiter","Deutsch Female");
+			}
+		}
+	}
+	
 }
