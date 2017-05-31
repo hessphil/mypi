13a8abc8280b9fc572ba5c3bc1428616f1392779
Schneider Mathias <Schneider.Mat@gmx.net>
2017-05-31 22:24:55 +0200

    - get requests works now asynchronically - ToDo: caller of method needs to react asynchronically on getPlayables()

--- backend/dataprovider/data/text.js
+++ backend/dataprovider/data/text.js
@@ -4,9 +4,9 @@ const Playable = require('./playable.js');
 
 class Text extends Playable {
 	constructor(text) {
-		if (this.constructor === Text) {
-			throw new Error("Can't instantiate abstract class!");
-		}
+		// if (this.constructor === Text) {
+			// throw new Error("Can't instantiate abstract class!");
+		// }
 		
 		super();
 		this.text = text
--- backend/dataprovider/datacrawler/NewsDataCrawler.js
+++ backend/dataprovider/datacrawler/NewsDataCrawler.js
@@ -1,9 +1,8 @@
 
 'use strict';
 const News = require('../data/news.js');
+var https = require('https');
 
-
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 class NewsDataCrawler{
 	//Variables
@@ -11,33 +10,16 @@ class NewsDataCrawler{
 	
 
 	 constructor(){	
-	this.relevantProvider = [];
-	this.keyWords=[];
+		this.relevantProvider = [];
+		this.keyWords=[];
+		
+		this.newsMap = {"spiegel":"https://newsapi.org/v1/articles?source=spiegel-online&sortBy=top&apiKey=298ff739e4364254a522a26fc88466da",
+		"bild":"https://newsapi.org/v1/articles?source=bild&sortBy=latest&apiKey=298ff739e4364254a522a26fc88466da",
+		"focus":"https://newsapi.org/v1/articles?source=focus&sortBy=top&apiKey=298ff739e4364254a522a26fc88466da",
+		"tagesspiegel":"https://newsapi.org/v1/articles?source=der-tagesspiegel&sortBy=latest&apiKey=298ff739e4364254a522a26fc88466da",
+		"zeit":"https://newsapi.org/v1/articles?source=die-zeit&sortBy=latest&apiKey=298ff739e4364254a522a26fc88466da"};
 	
-	this.newsMap = new Map([["spiegel","https://newsapi.org/v1/articles?source=spiegel-online&sortBy=top&apiKey=298ff739e4364254a522a26fc88466da"],
-	["bild","https://newsapi.org/v1/articles?source=bild&sortBy=latest&apiKey=298ff739e4364254a522a26fc88466da"],
-	["focus","https://newsapi.org/v1/articles?source=focus&sortBy=top&apiKey=298ff739e4364254a522a26fc88466da"],
-	["tagesspiegel","https://newsapi.org/v1/articles?source=der-tagesspiegel&sortBy=latest&apiKey=298ff739e4364254a522a26fc88466da"],
-	["zeit","https://newsapi.org/v1/articles?source=die-zeit&sortBy=latest&apiKey=298ff739e4364254a522a26fc88466da"]]);
-	}
-	
-	//Methods
-	// Create the XHR object.
-	createCORSRequest(method, url) 
-	{
-	  var xhr = new XMLHttpRequest();
-	  if ("withCredentials" in xhr) {
-		// XHR for Chrome/Firefox/Opera/Safari.
-		xhr.open(method, url, true);
-	  } else if (typeof XDomainRequest != "undefined") {
-		// XDomainRequest for IE.
-		xhr = new XDomainRequest();
-		xhr.open(method, url);
-	  } else {
-		// CORS not supported.
-		xhr = null;
-	  }
-	  return xhr;
+		this.newsList=[]
 	}
 
 	//SetRelevantProvider
@@ -54,40 +36,46 @@ class NewsDataCrawler{
 	//get news from each relevant provider
 	getPlayables ()
 	{
-		var playables=[];
 		if (this.relevantProvider.length > 0)
 		{
 			for (var provIndex=0; provIndex < this.relevantProvider.length; provIndex++)
 			{
+				console.log(this.relevantProvider[provIndex]);
+				console.log(this.newsMap[this.relevantProvider[provIndex]])
+				
+				
 				//get current provider url
 				var curProvider=this.newsMap[this.relevantProvider[provIndex]];	
 				
 				//get latest news
-				var jsonList=this.getTheLatest(curProvider);
-				
-				//filter news by keyword
-				var filteredNews=this.FilterNewsByKeyWords(jsonList);
-				
-				//add to playables list
-				var newPlayables=this.ConvertToPlayables(filteredNews, this.relevantProvider[provIndex]);
-				playables=playables.concat(newPlayables);
+				this.getTheLatest(curProvider,provIndex);
 			}
 		}
 		//if selected provider ==0 go through all providers
 		//check if keyword is in article description
+		
+		
 	}
 	//GetNews
-	getTheLatest(url){
-		var xhr = this.createCORSRequest('GET', url);
-		xhr.send();
-		// Response handlers.
-		xhr.onload = function() 
-		{
-		var string = xhr.responseText;
-		var temp = JSON.parse(string);
-		return temp;
-		};
-	}
+	getTheLatest(url,provIndex) {
+		https.get(url, (res) => {
+		  res.on('data', (d) => {
+			this.resp = d;
+			console.log('data:', d);
+			var temp = JSON.parse(this.resp);
+			//filter news by keyword
+			var filteredNews=this.FilterNewsByKeyWords(temp);
+			
+			
+			//add to playables list
+			var newNews=this.ConvertToNews(filteredNews, this.relevantProvider[provIndex]);
+			this.newsList=this.newsList.concat(newNews);
+		  });
+
+		}).on('error', (e) => {
+		  console.error(e);
+		});
+	  }
 	
 	//gets all news for keyword
 	FilterNewsByKeyWords(jsonList)
@@ -95,28 +83,32 @@ class NewsDataCrawler{
 		//filtered News
 		var filteredList=[];
 		
-		var arrayLength =jsonList.articles.length;
+		var arrayLength =jsonList['articles'].length;
 		for (var i = 0; i < arrayLength; i++)
 		{
 			//go through all keywords
 			for (var keyWordIndex = 0; keyWordIndex < this.keyWords.length; keyWordIndex++)
 			{
 				//check if keyword is in description
-				if(jsonList.articles[i].description.includes(keyWords[keyWordIndex]))
+				if(jsonList['articles'][i].description.includes(this.keyWords[keyWordIndex]))
 				{
 					//todo: mark keyword as found
-					
-					filteredList.push(jsonList.articles[i]);
+					console.log(jsonList['articles'][i])
+					filteredList.push(jsonList['articles'][i]);
 					break;
 				}
 			}
 		}
+		
+		return filteredList;
 	}
 	
-	ConvertToPlayables (filteredNews, providerName)
+	ConvertToNews (filteredNews, providerName)
 	{
 		var newsPlayAbles = [];
 		
+		console.log("filteredNews: " + filteredNews)
+		console.log("filteredNews length: " + filteredNews.length)
 		for (var newsIndex=0; newsIndex < filteredNews.length; newsIndex++)
 			{
 				var curNews = new News();
