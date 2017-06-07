012d2e807a11fe826a80293d3905886e6b25473b
hessphil <p.hess1@gmx.de>
2017-06-07 20:30:30 +0200

    add button:

--- utils/controller.js
+++ utils/controller.js
@@ -15,8 +15,8 @@ function getPlayablesFromServer()
 	
 	// Query data until we get a valid response
 	var x = document.cookie
-	console.log(x);
-	httpGet("http://localhost:8080/getPlayable/:token");
+	var url = "http://localhost:8080/getPlayable/" + x.split("=")[1];
+	console.log(httpGet(url));
 	
 }
 
